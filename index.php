f7df35f87e6113905b737736c38ac275cf2a0ab4
m38109 <m38109@gmail.com>
2024-07-10 11:19:57 +0800

    test

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    print "<p>Today is ".date("l F jS")."</p>\n";
+    ?>
+</body>
+</html>
